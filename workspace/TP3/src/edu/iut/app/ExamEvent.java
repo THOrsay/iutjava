82059fd8505c4db9e4743cd04054081af6806556
THOrsay <thomas.huppe@u-psud.fr>
2015-12-01 16:18:15 +0100

    TP3

---
+++ workspace/TP3/src/edu/iut/app/ExamEvent.java
@@ -0,0 +1,78 @@
+package edu.iut.app;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+public class ExamEvent {
+	public ExamEvent() {		
+	}
+	
+	public ExamEvent(Date date, Person person, ArrayList<Person> jury,
+					Classroom classRoom, ArrayList<Document> document) {
+		
+	}
+	
+	/** EX2: FAITES LES ACCESSEURS DES ATTRIBUTS, AJOUTER DES ATTRIBUTS **/
+	protected Date examDate;
+	protected Person student;
+	protected ArrayList<Person> jury;
+	protected Classroom classroom;
+	protected ArrayList<Document> documents;
+	
+	public Date getExamDate() {
+		return examDate;
+	}
+
+	public Person getStudent() {
+		return student;
+	}
+
+	public ArrayList<Person> getJury() {
+		return jury;
+	 
+		}
+
+	public Classroom getClassroom() {
+		return classroom;
+		
+	
+	}
+
+	public ArrayList<Document> getDocuments() {
+		return documents;
+	}
+	
+	public void add(ExamEvent e){
+
+		
+		
+	}
+
+	 
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* 
+
+Combien ça fait 1+1 ? 2 ? Non, 1. Tu vois toi et moi on parle ça fait 1. Alors oui dans notre monde 1+1=2
+2+2=4, mais c'est parce qu'on est selfish, on veut du pognon. Mais peut être que dans un autre
+monde 1+1=11, et ça c'est beau
+
+*/
